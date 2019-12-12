5c5f7a0dbbd6924e526630b0a8e51eb28e4c04ea
GwadaKing <davidmiolard@laposte.net>
2019-12-12 15:21:24 -0400

    V3 Priests watch over Boss and live to protect him

--- cultistWars.js
+++ cultistWars.js
@@ -13,14 +13,20 @@ for (let i = 0; i < height; i++) {
 
 // game loop
 while (true) {
-    let myBoss       = {};
-    let enemyBoss    = {};
-    let myUnits      = [];
-    let oppUnits     = [];
-    let neutralUnits = [];
-    let peons        = [];
-    let enemyPriests = [];
-    const numOfUnits = parseInt(readline()); // The total number of units on the board
+    let lockingEnemyPriest  = "no";
+    let myBoss              = {};
+    let enemyBoss           = {};
+    let myUnits             = [];
+    let oppUnits            = [];
+    let neutralUnits        = [];
+    let peons               = [];
+    let enemyPriests        = [];
+    let peon2Convert        = 777;
+    let enemy2Shoot         = 777;
+    let instructions        = "WAIT";
+    let convertInstructions = "no";
+    let defenseInstructions = "no";
+    const numOfUnits        = parseInt(readline()); // The total number of units on the board
     for (let i = 0; i < numOfUnits; i++) {
         var inputs = readline().split(' ');
         const unitId   = parseInt(inputs[0]); // The unit's ID
@@ -31,43 +37,72 @@ while (true) {
         const owner = parseInt(inputs[5]); // id of owner player
         // RETRIEVING DATAS
         // Retrieving raw datas
-        if (owner == 2) {
+        if (owner === 2) {
             neutralUnits.push({"id":unitId, "hp":hp, "x":x, "y":y});
         }
-        else if (owner == myId) {
+        else if (owner === myId) {
             if (unitType === 0) {
                 myUnits.push({"id":unitId, "hp":hp, "x":x, "y":y});
             }
-            else {
+            else if (unitType === 1)  {
                 myBoss = {"hp":hp, "x":x, "y":y};
             }
         }
-        else {
+        else if (owner !== 2 && owner !== myId) {
             if (unitType === 0) {
                 oppUnits.push({"id":unitId, "hp":hp, "x":x, "y":y});
             }
-            else {
+            else if (unitType === 1) {
                 enemyBoss = {"hp":hp, "x":x, "y":y};
             }
         }
     }
     // Stocking finally foreign entities(neutral cultist I call peons and enemy cultists I call enemy priests) with distances between each one and my Boss
     // Peons
-    for (let i = 0, l = peons.length; i < l; i++) {
-        peons[i] = {"id":neutralUnits[i].id, "hp":neutralUnits[i].hp, "x":neutralUnits[i].x, "y":neutralUnits[i].y, "distance":calcDistance(myBoss.x, myBoss.y, neutralUnits[i].x, neutralUnits[x].y)};
+    let distMin       = 777;
+    let closestPeonId = 777;
+    for (let j = 0, l = neutralUnits.length; j < l; j++) {
+        let dist2Peon = calcDistance(myBoss.x, myBoss.y, neutralUnits[j].x, neutralUnits[j].y);
+        if (dist2Peon < distMin) {
+            distMin       = dist2Peon;
+            closestPeonId = neutralUnits[j].id;
+        }
+        peons[j] = {"id":neutralUnits[j].id, "hp":neutralUnits[j].hp, "x":neutralUnits[j].x, "y":neutralUnits[j].y, "distance":dist2Peon};
     }
     // Enemy priests
-    for (let i = 0, l = oppUnits.length; i < l; i++) {
-        enemyPriests[i] = {"id":oppUnits[i].id, "hp":oppUnits[i].hp, "x":oppUnits[i].x, "y":oppUnits[i].y, "distance":calcDistance(myBoss.x, myBoss.y, oppUnits[i].x, oppUnits[x].y)};
+    for (let j = 0, l = oppUnits.length; j < l; j++) {
+        let dist2Priest = calcDistance(myBoss.x, myBoss.y, oppUnits[j].x, oppUnits[j].y);
+        enemyPriests[j] = {"id":oppUnits[j].id, "hp":oppUnits[j].hp, "x":oppUnits[j].x, "y":oppUnits[j].y, "distance":dist2Priest};
     }
-    // Enemy Boss
-    let distBetweenBosses = calcDistance(myBoss.x, myBoss.y, enemyBoss.x, enemyBoss.y);
     
-    peons.apply(function(o) { return Math.max(o.distance)});
+    let distBetweenBosses   = calcDistance(myBoss.x, myBoss.y, enemyBoss.x, enemyBoss.y);
+    let closestPeonDistance = Math.min.apply(Math, peons.map(function(o) { return o.distance}));
+    let closestEnemyDistance = 777;
     
-    // WAIT | unitId MOVE x y | unitId SHOOT target| unitId CONVERT target
+    // Checking if Bossman is threatened by an enemy priest (range <= 7)
+    if (oppUnits.length > 0) {
+        defenseInstructions = defendTheBoss(myUnits, enemyPriests);
+        lockingEnemyPriest = shootEnemy(myUnits, oppUnits);
+    }
+    // Open fire if a priest is under range
+    if (lockingEnemyPriest != "no priests around my units" && lockingEnemyPriest != "no") {
+        instructions = lockingEnemyPriest;
+    }
+    
+    // Defend the boss if a priest is threatening him
+    if (defenseInstructions != "no priests around boss" && defenseInstructions != "no") {
+        instructions = defendTheBoss(myUnits, enemyPriests);
+    }
+    // Every 2 turns, boss tries to convert a peon
+    if (tour %2 === 0 && peons.length > 0) {
+        // Convert a peon 
+        instructions = convertPeons(myBoss, peons, myId);
+    }
+    
     console.log(instructions);
+    tour++;
 }
+
 /**
  * Afficher la grille du jeu
  * */
@@ -84,5 +119,79 @@ function displayMap(grid) {
  * */
 function calcDistance(x, y, x2, y2) {
     return Math.round( (x2 - x) + (y2 - y) );
+}
+
+/**
+ * shootEnemy
+ * */
+ function shootEnemy(myUnits, oppUnits) {
+     let targetId              = 777;
+     let sourceId              = 777;
+     for (let i = 0, l = myUnits.length; i < l; i++) {
+        let myPriestId            = myUnits[i].id;
+        let closestPriestDistance = 8;
+        for (let j = 0, l = oppUnits.length; j < l; j++) {
+            let enemyPriestId       = oppUnits[j].id;
+            let enemyPriestDistance = calcDistance(myUnits[i].x, myUnits[i].y, oppUnits[j].x, oppUnits[j].y);
+            // Is enemy in the shoot range ?
+            if (enemyPriestDistance < 8) {
+               if (enemyPriestDistance < closestPriestDistance) {
+                    closestPriestDistance = enemyPriestDistance;
+                    targetId              = enemyPriestId;
+                    sourceId              = myPriestId;
+                } 
+            }
+        }
+    }
+    if (sourceId == 777) { return "no priests around my units" } else return sourceId+" SHOOT "+targetId;
+ }
+ 
+ /**
+  * convertPeons
+  * 1 check if there is a peon in adjacent tiles
+  * 2 if none walks towards the closest peon
+  * */
+function convertPeons(myBoss, peons, myId, closestPeonId) {
+    for (let j = 0, l = peons.length; j < l; j++) {
+        if (peons[j].x == (myBoss.x)+1 && peons[j].y == myBoss.y || 
+            peons[j].x == (myBoss.x)-1 && peons[j].y == myBoss.y ||
+            peons[j].x == myBoss.x && peons[j].y == (myBoss.y)+1 ||
+            peons[j].x == myBoss.x && peons[j].y == (myBoss.y)-1 ) {
+            let peon2Convert = peons[j].id;
+            instructions = myId+" CONVERT "+peon2Convert;
+            return instructions;
+        }
+    }
+    instructions = myId+" CONVERT "+closestPeonId;
+    return instructions;
+}
+
+/**
+ * Defend The Boss
+ * */
+function defendTheBoss(myUnits, enemyPriests) {
+    let defenderId           = 777;
+    let closestEnemyDistance = Math.min.apply(Math, enemyPriests.map(function(o) { return o.distance}));
+    for (let j = 0, l = enemyPriests.length; j < l; j++) {
+        if (enemyPriests[j].distance == closestEnemyDistance) {
+            if (closestEnemyDistance <= 7) {
+                // Bossman is threatened, priority is finding closest priest to defend
+                enemy2Shoot     = enemyPriests[j].id;
+                let minDistance = 777;
+                for (let k = 0, o = myUnits.length; k < o; k++) {
+                    let distance2Enemy = calcDistance(myUnits[k].x, myUnits[k].y, enemyPriests[j].x, enemyPriests[j].y);
+                    if (distance2Enemy < minDistance) {
+                        minDistance = distance2Enemy;
+                        defenderId  = myUnits[k].id;
+                    }
+                }
+            } 
+        }
+    }
+    if (defenderId == 777) {
+        return "no priests around boss";
+    }
+    else instructions = defenderId+" SHOOT "+enemy2Shoot;
+    return instructions;
 }
          
